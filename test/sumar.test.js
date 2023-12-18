92c1f6246eea7442f4e7fa0676de1afe5b1d0351
aronresni <aronresni@gmail.com>
2023-12-18 20:14:41 -0300

    upload

--- test/sumar.test.js
+++ test/sumar.test.js
@@ -5,4 +5,11 @@ describe('La funcion sumar', () => {
         expect(typeof sumar).toBe("function")
         expect(typeof sumar).not.toBe("number")
     })
+    it('Debe arrojar un error en caso de recibir algo que no sea un numero', () => {
+        expect(sumar("holis", "tukis")),toThrow("Los parametros deben ser numeros")
+    });
+    it('Debe arrojar un error en caso de recibir algo que no sea un numero', () => {
+        expect(sumar("holis", "tukis")),toThrow("Los parametros deben ser numeros")
+    });
+    
 });
